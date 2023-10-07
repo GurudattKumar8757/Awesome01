fe5823a2e4fe41ed6c9f4e039fbac961eea1fd52
GurudattKumar8757 <gurudattkumar8757@gmail.com>
2023-10-07 17:57:49 +0530

    Project02

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-
-import {View, Text, SafeAreaView, Button} from 'react-native';
+import {View, Text, SafeAreaView, ScrollView} from "react-native";
+import React from "react";
+import FlatCards from "./components/FlatCards";
+import ElevatedCards from "./components/ElevatedCards";
+import FancyCard from "./components/FancyCard";
+import ActionCard from "./components/ActionCard";
+import ContactList from "./components/ContactList";
 
 const App = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Hello World !</Text>
-        <Text>Hello World !</Text>
-        <Text>Hello World !</Text>
-        <Text>Hello World !</Text>
-        <Text>Hello World !</Text>
-        <Button title="Hello" color="red" />
-      </View>
+      <ScrollView>
+        <FlatCards />
+        <ElevatedCards />
+        <FancyCard />
+        <ActionCard />
+        <ContactList />
+      </ScrollView>
     </SafeAreaView>
   );
 };
